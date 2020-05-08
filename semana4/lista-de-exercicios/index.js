112acaee582ef51f64c7a656dab9f0c33fffa777
LeonamMoura <leo99.music@gmail.com>
2020-05-08 16:53:17 -0300

    Questoes 1 e 2 semana4-projeto

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,17 @@
+//INTERPRETAÇÃO DE CÓDIGO
+
+//1. 
+// -> cria uma função chamada ConversorDeMoeda e recebe como parâmetro valorEmDolar
+// -> cria uma variável const chamada cotacao que recebe o valor informado pelo usuario através do prompt, já formatado para número com Number()
+// -> retorna uma string concatenando com o resuldado de valorEmDolar vezes cotacao
+// -> fora da função é criada uma variável const chamada meuDinheiro
+// -> meuDinheiro recebe o valor da função com parâmetro valorEmDolar = 100
+// -> para saber o resultado no console é preciso saber o que o usuário digita no valor da cotacao
+
+
+//2. 
+// -> cria uma função chamada investeDinheiro que recebe como parâmetros tipoDeInvestimento e valor
+// -> cria uma variável let chamada valorAposInvestimento
+// -> usa a condicional Switch Case no tipo de investimento
+// -> primeiro caso, caso o tipoDeInvestimento seja uma string = "Poupança", valorAposInvestimento recebe valor vezes 1.03 e sai do switch case com break
+// -> 
