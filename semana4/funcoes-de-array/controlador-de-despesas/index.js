98e28263598bedecc2a140744c574ee2bdd2235c
LeonamMoura <leo99.music@gmail.com>
2020-05-10 16:46:09 -0300

    Exercicio ainda incompleto, mas com algumas implementações

--- semana4/funcoes-de-array/controlador-de-despesas/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -1,6 +1,25 @@
-const sessaoDeCadastro = document.getElementById("sessao-cadastro")
+//Sessão de Cadastro de Despesas
+let listaDeDespesas = []
 
-function cadastraNovaDespesa() {
-    const novaDespesa = {valor: document.getElementById("valor-da-despesa").value, tipo: document.getElementById("tipo-da-despesa").value}
-    console.log(novaDespesa)
+function cadastraDespesa() {
+    event.preventDefault()
+    let valorDespesa = document.getElementById("valor-da-despesa").value
+    let tipoDespesa = document.getElementById("tipo-da-despesa").value
+    let descricaoDespesa = document.getElementById("descricao-da-despesa").value
+
+    let despesa = {valor: Number(valorDespesa), tipo: tipoDespesa, descricao: descricaoDespesa}
+
+    listaDeDespesas.push(despesa)
+}
+
+
+//Sessão de Despesas Detalhadas
+function imprimeDespesas() {
+    event.preventDefault()
+    let tipoDetalhada = document.getElementById("tipo-da-despesa-detalhada").value
+    let valMinimo = document.getElementById("valor-minimo").value
+    let valMaximo = document.getElementById("valor-maximo").value
+    let sessaoDetalhadas = document.getElementById("sessao-detalhadas")
+
+    sessaoDetalhadas.innerHTML += "<p>Olá, uma nova Despesa foi cadastrada!</p>"
 }
