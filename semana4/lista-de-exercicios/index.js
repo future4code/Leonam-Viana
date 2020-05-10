36de6c3fedc86721819e00024c9e742f03e2a3bd
LeonamMoura <leo99.music@gmail.com>
2020-05-10 18:49:28 -0300

    Terceira Questão

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,17 +1,35 @@
-//INTERPRETAÇÃO DE CÓDIGO
+//=============================== INTERPRETAÇÃO DE CÓDIGO ===============================
+/*
+let primeiraQuestao = () => {
+    
+        Esta função recebe um valor como parâmetro;
+        Este parâmetro é inserido como 100 na const meuDinheiro;
+        A function pede que o usuário informe o valor da cotação;
+        E multiplica o valorEmDolar(que é 100) pelo valor da cotação inserido pelo usuário;
+        Esta função vai imprimir qualquer valor digitado pelo usuario no prompt multiplicado por 100;
+    
+}
 
-//1. 
-// -> cria uma função chamada ConversorDeMoeda e recebe como parâmetro valorEmDolar
-// -> cria uma variável const chamada cotacao que recebe o valor informado pelo usuario através do prompt, já formatado para número com Number()
-// -> retorna uma string concatenando com o resuldado de valorEmDolar vezes cotacao
-// -> fora da função é criada uma variável const chamada meuDinheiro
-// -> meuDinheiro recebe o valor da função com parâmetro valorEmDolar = 100
-// -> para saber o resultado no console é preciso saber o que o usuário digita no valor da cotacao
+let segundaQuestao = () => {
+    
+        Esta function recebe dois parâmetros, que são tipoDeInvestimento e valor;
+        Cria uma variável let chamada valorAposInvestimento;
+        Com o Switch case, a function oferece 5 resultados diferentes que são:
+        Poupança, Renda Fixa, CDB, Ações e um default para caso o tipoDeInvestimento não seja nenhuma das 4 primeiras;
+        Em todos os cases a let valorAposInvestimento é multiplicada pela taxa de juros de cada tipoDeInvestimento;
+        Após o Switch case, a função retorna o valorAposInvestimento que é resultado do switch case;
+        novoMontante e segundoMontante são variáveis que receberam os valores da função com os parâmetros já estabelecidos;
+        o primeiro console.log mostrará 165, o segundo console.log mostra TIPO DE INVESTIMENTO INFORMADO INCORRETO, que é o default do switch case;
+    
+}
 
-
-//2. 
-// -> cria uma função chamada investeDinheiro que recebe como parâmetros tipoDeInvestimento e valor
-// -> cria uma variável let chamada valorAposInvestimento
-// -> usa a condicional Switch Case no tipo de investimento
-// -> primeiro caso, caso o tipoDeInvestimento seja uma string = "Poupança", valorAposInvestimento recebe valor vezes 1.03 e sai do switch case com break
-// -> 
+let terceiraQuestao = () => {
+    
+        Cria três arrays/listas, sendo a primeira preenchida por números e as outras duas vazias;
+        Cria um laço onde todos os numeros pares serão jogados na array1 e os ímpares na array2;
+        Fora da function tem três mensagens mostradas no console;
+        A primeira é a quantidade total de números da array numeros;
+        A segunda é a quantidade total de números da array1
+        A terceira é a quantidade total de números da array2
+    
+}
