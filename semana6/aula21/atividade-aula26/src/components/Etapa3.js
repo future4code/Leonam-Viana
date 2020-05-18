a58139d7d722b1ba8b483cf6210bde0573fd35db
LeonamMoura <leo99.music@gmail.com>
2020-05-18 16:36:50 -0300

    Entregando atividade da tarde - aula21

---
+++ semana6/aula21/atividade-aula26/src/components/Etapa3.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class Etapa3 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+
+                <h3>5. Por que você não terminou um curso de graduação ?</h3>
+                <input />
+
+                <h3>6. Você fez algum curso complementar ?</h3>
+                <select>
+                    <option>Curso Técnico</option>
+                    <option>Curso de Inglês</option>
+                    <option>Não fiz curso complementar</option>
+                </select>
+            </div>
+        )
+    }
+}
+
+export default Etapa3;
---
+++ semana6/aula21/atividade-aula26/src/components/Final.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Final extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>O FORMULÁRIO ACABOU</h1>
+
+                <h3>Muito obrigado por participar! Entraremos em contato!</h3>
+            </div>
+        )
+    }
+}
+
+export default Final;
