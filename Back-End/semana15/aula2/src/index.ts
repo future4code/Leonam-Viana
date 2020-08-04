cf7fed4c4bdd7b1f3486e6586f8d3647c3ec7bb5
LeonamMoura <leo99.music@gmail.com>
2020-08-04 16:33:03 -0300

    Entregando exercícios resolvidos até agora

---
+++ Back-End/semana15/aula2/src/index.ts
@@ -0,0 +1,7 @@
+//Exercício 1
+  //a. Não! Pois a keyword private faz com que essa variável só seja acessada de dentro da classe
+
+  //b. Nenhuma, acredito que este console.log só é feito quando chamamos o constructor da superclasse, que por sua vez só é chamado quando criamos uma subclasse a partir desta superclasse!
+
+//Exercício 2
+  //a. 
